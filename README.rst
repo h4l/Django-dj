63f8564229c9c6bc8903b49a4c1d7b7408f0c6d3
Hal Blackburn <hal.blackburn@gmail.com>
2012-06-08 15:49:58 +0100

    Created package metadata for 0.1.0 release.

---
+++ README.rst
@@ -0,0 +1,34 @@
+Django-dj
+=========
+
+Django-dj is a console command which removes the need to use Django's 
+``manage.py`` script. 
+
+Old way
+-------
+
+::
+
+  $ django-project> python manage.py runserver/syncdb/etc
+
+::
+
+  $ django-project/some/dir> python ../../manage.py runserver/syncdb/etc
+
+New Way
+-------
+
+::
+
+  $ django-project> dj runserver/syncdb/etc
+
+::
+
+  $ django-project/some/dir> dj runserver/syncdb/etc
+
+Instead of carefully referencing ``manage.py`` from whichever directory you
+happen to be in, ``dj`` works anywhere in or under your Django project 
+directory. You can even completely remove your ``manage.py`` file if you wish.
+
+``dj`` identifies Django projects by looking for directories containing 
+``__init__.py``, ``settings.py`` and ``urls.py``.
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from os import path
+
+README = path.join(path.dirname(__file__), "README.rst")
+with open(README) as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="Django-dj",
+    version="0.1.0",
+    description=("A short command which replaces calls to Django's manage.py "
+            "scripts"),
+    author="Hal Blackburn",
+    author_email="hwtb2@caret.cam.ac.uk",
+    url="https://github.com/h4l/Django-dj",
+    scripts=["dj"],
+    requires=[
+        "Django (>=1.4)"
+    ],
+    license="BSD",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    long_description=LONG_DESCRIPTION,
+)
